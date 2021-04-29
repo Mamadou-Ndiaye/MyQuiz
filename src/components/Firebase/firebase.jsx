0b7770c02a633b2e293fc8afc4c01c087e680370
Mamadou_Ndiaye <ndiamamadou@gmail.com>
2021-04-29 16:17:03 +0200

    installation de firebase et onfiguration

---
+++ src/components/Firebase/firebase.jsx
@@ -0,0 +1,22 @@
+import app from 'firebase/app';
+
+import React, { Component } from 'react'
+
+
+const config = {
+    apiKey: "AIzaSyApKDcihSLQEzHH267dFCwlZf9dqnja_bM",
+    authDomain: "myquiz-5d316.firebaseapp.com",
+    projectId: "myquiz-5d316",
+    storageBucket: "myquiz-5d316.appspot.com",
+    messagingSenderId: "67614049490",
+    appId: "1:67614049490:web:d9980cc285fbb617c2590b"
+  };
+
+class Firebase  {
+    constructor() {
+       app.initializeApp(config);
+    }
+    
+  
+}
+export default Firebase;
