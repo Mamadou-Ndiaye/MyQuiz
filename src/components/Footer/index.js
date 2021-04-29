b509a6b3d821568fb12232cacdebe283b8ca81cd
Mamadou_Ndiaye <ndiamamadou@gmail.com>
2021-04-29 13:45:37 +0200

    structure du projet component image

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+
+import React, {useState} from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-container">
+            <p>Projet realisé par Mamadou Ndiaye 2021</p>
+            <p>Les icones Wolverine , Iron-man,Spider-man,batman sont prises sur le site iconFinder.com</p>
+
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+
+const Header = () => {
+  return (
+    <header>
+         <div className="banner-container">
+             <h1><a href="/">My QUIZ</a></h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header
